0e45864e4a81aeaa4526776c4ae7688c0796a05d
Casey Ross <caseyross003@gmail.com>
2017-10-16 09:39:51 -0500

    Update key path

--- modules/mean-stack-config/vars.tf
+++ modules/mean-stack-config/vars.tf
@@ -1,7 +1,7 @@
 variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
 variable "ssh_private_key_path" {
-  default="userdata/rsa-key-MEAN.ssh"
+  default="userdata/keys/rsa-key-MEAN.ssh"
 }
 variable "mean-stack-public-ip" {}
 variable "instance_count" {}
